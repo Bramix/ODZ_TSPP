4766acd0347f134a938710af1a09e8df1eead319
Bramix <bramixtop@gmail.com>
2020-03-14 16:49:01 +0200

    Initial commit

---
+++ ODZ_TSPP/Forms/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ODZ_TSPP
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
