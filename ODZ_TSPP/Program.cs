7475e61899e8c31fc6bd3911a8bd74248f2578dd
Bramix <bramixtop@gmail.com>
2020-03-28 22:02:37 +0200

    Start sql script has been added.

--- ODZ_TSPP/Program.cs
+++ ODZ_TSPP/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ODZ_TSPP
 {
@@ -9,6 +11,14 @@ namespace ODZ_TSPP
         [STAThread]
         public static void Main()
         {
+            String script = File.ReadAllText( "..\\..\\Start.sql");
+            using (MySqlConnection conn = new MySqlConnection(Configs.connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(script, conn))
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
