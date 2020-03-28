ec45fa24c72e3c37c4d42018771fcf63663ec16c
Bramix <bramixtop@gmail.com>
2020-03-28 18:17:49 +0200

    Start sql script has been added.

---
+++ ODZ_TSPP/Start.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL,
+  `SecondName` varchar(50) NOT NULL,
+  `Mark` DOUBLE NOT NULL,
+  `NumberOfSchool` INTEGER NOT NULL,
+   PRIMARY KEY (id)
+);
+
+
+INSERT INTO `users` (`FirstName`, `SecondName`, `Mark`, `NumberOfSchool`) VALUES
+( 'Go', 'GO', '100', '200'),
+( 'Bob', 'Anna', '200', '444444');
+
+commit;
