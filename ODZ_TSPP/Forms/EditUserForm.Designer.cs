661fd779317867a70dcbf1b859dde6db07f60f32
Bramix <bramixtop@gmail.com>
2020-04-27 01:56:25 +0300

    UserForm updating

--- ODZ_TSPP/Forms/EditUserForm.Designer.cs
+++ ODZ_TSPP/Forms/EditUserForm.Designer.cs
@@ -32,8 +32,6 @@
             this.txtStreet = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.pnlError = new System.Windows.Forms.Panel();
-            this.lblError = new System.Windows.Forms.Label();
             this.btn1 = new System.Windows.Forms.Button();
             this.Mobile = new System.Windows.Forms.Label();
             this.txtLastName = new System.Windows.Forms.TextBox();
@@ -45,7 +43,6 @@
             this.tmrError = new System.Windows.Forms.Timer(this.components);
             this.pnlHeader.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.pnlError.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlHeader
@@ -56,7 +53,7 @@
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
             this.pnlHeader.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(750, 74);
+            this.pnlHeader.Size = new System.Drawing.Size(532, 57);
             this.pnlHeader.TabIndex = 1;
             // 
             // lblTitle
@@ -64,12 +61,13 @@
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(267, 9);
+            this.lblTitle.Location = new System.Drawing.Point(160, 9);
             this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(42, 45);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "{}";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // panel1
             // 
@@ -82,7 +80,6 @@
             this.panel1.Controls.Add(this.txtStreet);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.pnlError);
             this.panel1.Controls.Add(this.btn1);
             this.panel1.Controls.Add(this.Mobile);
             this.panel1.Controls.Add(this.txtLastName);
@@ -91,62 +88,62 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtPhoneNumber);
-            this.panel1.Location = new System.Drawing.Point(37, 96);
+            this.panel1.Location = new System.Drawing.Point(54, 59);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(648, 621);
+            this.panel1.Size = new System.Drawing.Size(430, 620);
             this.panel1.TabIndex = 2;
             // 
             // txtNumberOfFlat
             // 
-            this.txtNumberOfFlat.Location = new System.Drawing.Point(51, 513);
+            this.txtNumberOfFlat.Location = new System.Drawing.Point(18, 509);
             this.txtNumberOfFlat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNumberOfFlat.Name = "txtNumberOfFlat";
-            this.txtNumberOfFlat.Size = new System.Drawing.Size(542, 26);
+            this.txtNumberOfFlat.Size = new System.Drawing.Size(380, 26);
             this.txtNumberOfFlat.TabIndex = 17;
             // 
             // label6
             // 
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.label6.Location = new System.Drawing.Point(51, 477);
+            this.label6.Location = new System.Drawing.Point(18, 473);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(542, 31);
+            this.label6.Size = new System.Drawing.Size(380, 31);
             this.label6.TabIndex = 16;
             this.label6.Text = "Number of the flat";
             this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // txtCity
             // 
-            this.txtCity.Location = new System.Drawing.Point(51, 280);
+            this.txtCity.Location = new System.Drawing.Point(18, 272);
             this.txtCity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCity.Name = "txtCity";
-            this.txtCity.Size = new System.Drawing.Size(542, 26);
+            this.txtCity.Size = new System.Drawing.Size(380, 26);
             this.txtCity.TabIndex = 15;
             // 
             // txtNumberOfTheHouse
             // 
-            this.txtNumberOfTheHouse.Location = new System.Drawing.Point(51, 435);
+            this.txtNumberOfTheHouse.Location = new System.Drawing.Point(18, 428);
             this.txtNumberOfTheHouse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNumberOfTheHouse.Name = "txtNumberOfTheHouse";
-            this.txtNumberOfTheHouse.Size = new System.Drawing.Size(542, 26);
+            this.txtNumberOfTheHouse.Size = new System.Drawing.Size(380, 26);
             this.txtNumberOfTheHouse.TabIndex = 14;
             // 
             // txtStreet
             // 
-            this.txtStreet.Location = new System.Drawing.Point(51, 355);
+            this.txtStreet.Location = new System.Drawing.Point(18, 350);
             this.txtStreet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtStreet.Name = "txtStreet";
-            this.txtStreet.Size = new System.Drawing.Size(542, 26);
+            this.txtStreet.Size = new System.Drawing.Size(380, 26);
             this.txtStreet.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.label5.Location = new System.Drawing.Point(51, 399);
+            this.label5.Location = new System.Drawing.Point(18, 399);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(542, 31);
+            this.label5.Size = new System.Drawing.Size(380, 24);
             this.label5.TabIndex = 12;
             this.label5.Text = "Number of the house";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -154,38 +151,13 @@
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.label4.Location = new System.Drawing.Point(51, 325);
+            this.label4.Location = new System.Drawing.Point(18, 320);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(542, 25);
+            this.label4.Size = new System.Drawing.Size(380, 25);
             this.label4.TabIndex = 11;
             this.label4.Text = "Street\r\n";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // pnlError
-            // 
-            this.pnlError.BackColor = System.Drawing.Color.Tomato;
-            this.pnlError.Controls.Add(this.lblError);
-            this.pnlError.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlError.Location = new System.Drawing.Point(0, 0);
-            this.pnlError.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.pnlError.Name = "pnlError";
-            this.pnlError.Size = new System.Drawing.Size(648, 40);
-            this.pnlError.TabIndex = 10;
-            this.pnlError.Visible = false;
-            // 
-            // lblError
-            // 
-            this.lblError.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblError.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.lblError.ForeColor = System.Drawing.Color.White;
-            this.lblError.Location = new System.Drawing.Point(0, 0);
-            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(648, 40);
-            this.lblError.TabIndex = 2;
-            this.lblError.Text = "First Name";
-            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btn1
             // 
@@ -193,10 +165,10 @@
             this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btn1.ForeColor = System.Drawing.Color.White;
-            this.btn1.Location = new System.Drawing.Point(51, 565);
+            this.btn1.Location = new System.Drawing.Point(18, 558);
             this.btn1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(171, 51);
+            this.btn1.Size = new System.Drawing.Size(160, 48);
             this.btn1.TabIndex = 8;
             this.btn1.Text = "Save";
             this.btn1.UseVisualStyleBackColor = false;
@@ -205,49 +177,49 @@
             // Mobile
             // 
             this.Mobile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.Mobile.Location = new System.Drawing.Point(51, 250);
+            this.Mobile.Location = new System.Drawing.Point(18, 242);
             this.Mobile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Mobile.Name = "Mobile";
-            this.Mobile.Size = new System.Drawing.Size(542, 25);
+            this.Mobile.Size = new System.Drawing.Size(380, 25);
             this.Mobile.TabIndex = 7;
             this.Mobile.Text = "City\r\n";
             this.Mobile.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(51, 70);
+            this.txtLastName.Location = new System.Drawing.Point(18, 47);
             this.txtLastName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(542, 26);
+            this.txtLastName.Size = new System.Drawing.Size(380, 26);
             this.txtLastName.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.label3.Location = new System.Drawing.Point(51, 189);
+            this.label3.Location = new System.Drawing.Point(18, 165);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(542, 25);
+            this.label3.Size = new System.Drawing.Size(380, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Phone Number";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // txtYearOfConnection
             // 
-            this.txtYearOfConnection.Location = new System.Drawing.Point(51, 145);
+            this.txtYearOfConnection.Location = new System.Drawing.Point(18, 121);
             this.txtYearOfConnection.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtYearOfConnection.Name = "txtYearOfConnection";
-            this.txtYearOfConnection.Size = new System.Drawing.Size(542, 26);
+            this.txtYearOfConnection.Size = new System.Drawing.Size(380, 26);
             this.txtYearOfConnection.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.label2.Location = new System.Drawing.Point(51, 40);
+            this.label2.Location = new System.Drawing.Point(18, 17);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(542, 25);
+            this.label2.Size = new System.Drawing.Size(380, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Last Name";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -255,20 +227,20 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.label1.Location = new System.Drawing.Point(51, 115);
+            this.label1.Location = new System.Drawing.Point(18, 91);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(542, 25);
+            this.label1.Size = new System.Drawing.Size(380, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Year of Connection";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(51, 219);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(18, 195);
             this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(542, 26);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(380, 26);
             this.txtPhoneNumber.TabIndex = 4;
             // 
             // tmrError
@@ -280,7 +252,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(750, 829);
+            this.ClientSize = new System.Drawing.Size(532, 679);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -292,7 +264,6 @@
             this.pnlHeader.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.pnlError.ResumeLayout(false);
             this.ResumeLayout(false);
         }
 
@@ -303,11 +274,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label Mobile;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel pnlError;
         private System.Windows.Forms.Panel pnlHeader;
         private System.Windows.Forms.Timer tmrError;
         private System.Windows.Forms.TextBox txtCity;
