439860cc6d103c0c447eeebf720474a995b7f73f
Bramix <bramixtop@gmail.com>
2020-05-09 18:07:24 +0300

    SearchForm added

---
+++ ODZ_TSPP/Forms/SearchForm.Designer.cs
@@ -0,0 +1,302 @@
+﻿using System.ComponentModel;
+
+namespace ODZ_TSPP
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fields = new System.Windows.Forms.ComboBox();
+            this.value = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.typeOfOperation = new System.Windows.Forms.ComboBox();
+            this.AndOr = new System.Windows.Forms.ComboBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Fields
+            // 
+            this.Fields.FormattingEnabled = true;
+            this.Fields.Items.AddRange(new object[] {"SecondName", "YearOfConnection", "PhoneNumber", "City", "Supruna", "NumberOfHouse", "NumberOfFlat"});
+            this.Fields.Location = new System.Drawing.Point(40, 29);
+            this.Fields.Margin = new System.Windows.Forms.Padding(2);
+            this.Fields.Name = "Fields";
+            this.Fields.Size = new System.Drawing.Size(265, 28);
+            this.Fields.TabIndex = 0;
+            this.Fields.Text = "Searching field";
+            // 
+            // value
+            // 
+            this.value.Location = new System.Drawing.Point(40, 96);
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(265, 26);
+            this.value.TabIndex = 2;
+            this.value.Text = "Searching value";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 463);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 463);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 463);
+            this.panel3.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel4.Location = new System.Drawing.Point(339, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 463);
+            this.panel4.TabIndex = 11;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(10, 453);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(329, 10);
+            this.panel5.TabIndex = 12;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel6.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel6.Location = new System.Drawing.Point(319, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(10, 10);
+            this.panel6.TabIndex = 11;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (71)))), ((int) (((byte) (160)))));
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(10, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(329, 10);
+            this.panel7.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(76, 625);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 38);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(250, 615);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 38);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Filter";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(40, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 38);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Add filter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.AddFilter);
+            // 
+            // typeOfOperation
+            // 
+            this.typeOfOperation.FormattingEnabled = true;
+            this.typeOfOperation.Items.AddRange(new object[] {"=", "!=", "LIKE", "<", ">", "<=", ">="});
+            this.typeOfOperation.Location = new System.Drawing.Point(40, 62);
+            this.typeOfOperation.Name = "typeOfOperation";
+            this.typeOfOperation.Size = new System.Drawing.Size(265, 28);
+            this.typeOfOperation.TabIndex = 28;
+            this.typeOfOperation.Text = "Searching operation";
+            // 
+            // AndOr
+            // 
+            this.AndOr.AutoCompleteCustomSource.AddRange(new string[] {"AND", "OR"});
+            this.AndOr.FormattingEnabled = true;
+            this.AndOr.Items.AddRange(new object[] {"Or", "And"});
+            this.AndOr.Location = new System.Drawing.Point(40, 128);
+            this.AndOr.Name = "AndOr";
+            this.AndOr.Size = new System.Drawing.Size(265, 28);
+            this.AndOr.TabIndex = 30;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(40, 206);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(265, 198);
+            this.textBox.TabIndex = 31;
+            this.textBox.Text = "SELECT * FROM users u join address a on u.Id = a.UserId";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(40, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 38);
+            this.button4.TabIndex = 32;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Cancel);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.BackColor = System.Drawing.Color.SeaGreen;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(193, 409);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 38);
+            this.button5.TabIndex = 33;
+            this.button5.Text = "Filter";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Filter);
+            // 
+            // SearchForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(349, 463);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.AndOr);
+            this.Controls.Add(this.typeOfOperation);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.value);
+            this.Controls.Add(this.Fields);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Location = new System.Drawing.Point(22, 22);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox AndOr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox Fields;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ComboBox typeOfOperation;
+        private System.Windows.Forms.TextBox value;
+
+        #endregion
+    }
+}
